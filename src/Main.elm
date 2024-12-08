56f88c25f83aea34ac521bf056a3d3d808c3118f
Nathan Morrison <me@nathanmorrison.net>
2024-12-08 10:02:41 -0600

    Render header in Elm app
    This commit moves the header into the Elm app, so that the tutorial
    can be placed in line with it.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,8 @@
 module Main exposing ( main )
 
 import Html.Styled as HS
+import Html.Styled.Attributes as HSA
+import Css
 import Browser as B
 
 import Space
@@ -9,6 +11,17 @@ import SpaceCommand as SC
 main : Program () Space.Model SC.Message
 main = B.sandbox {
         init = SC.init
-      , view = HS.toUnstyled << SC.view
+      , view = HS.toUnstyled << viewWithHeader
       , update = SC.update
     }
+
+viewWithHeader : Space.Model -> HS.Html SC.Message
+viewWithHeader model =
+    HS.div []
+        [ HS.header []
+            [ HS.h1 
+                [ HSA.css [ Css.marginLeft (Css.rem 2) ] ]
+                [ HS.text "Fractal Play" ]
+            ]
+        , SC.view model
+        ]
